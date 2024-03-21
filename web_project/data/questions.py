794f16cb16869f4a996834ecc114f1e25b354922
Sasha_Morkovkin <sashagus2008@mail.ru>
2024-03-21 22:33:48 +0300

    first_commit

---
+++ web_project/data/questions.py
@@ -0,0 +1,15 @@
+from .db_session import SqlAlchemyBase
+import sqlalchemy
+from sqlalchemy import orm
+from sqlalchemy_serializer import SerializerMixin
+
+
+class Questions(SqlAlchemyBase, SerializerMixin):
+    __tablename__ = 'questions'
+
+    id = sqlalchemy.Column(sqlalchemy.Integer,
+                           primary_key=True, autoincrement=True)
+    its_user = sqlalchemy.Column(sqlalchemy.Integer,
+                                    sqlalchemy.ForeignKey("quezes.id"))
+
+    user = orm.relationship('Quezes')
---
+++ web_project/data/quezes.py
@@ -0,0 +1,15 @@
+from .db_session import SqlAlchemyBase
+import sqlalchemy
+from sqlalchemy import orm
+from sqlalchemy_serializer import SerializerMixin
+
+
+class Quezes(SqlAlchemyBase, SerializerMixin):
+    __tablename__ = 'quezes'
+
+    id = sqlalchemy.Column(sqlalchemy.Integer,
+                           primary_key=True, autoincrement=True)
+    its_user = sqlalchemy.Column(sqlalchemy.Integer,
+                                    sqlalchemy.ForeignKey("users.id"))
+
+    user = orm.relationship('User')
