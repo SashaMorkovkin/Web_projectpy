85847e5c51045b7e127d15a5670082c7ae208668
Aleksander_Morkovkin <sashagus2008@,ail.ru>
2024-04-21 22:43:22 +0300

    vk_bot_branch_0.3

--- vk_bot/vk_bot_api.py
+++ vk_bot/vk_bot_api.py
@@ -35,22 +35,26 @@ def bot():
     )
     for event in longpoll.listen():
         vk = vk_session.get_api()
-        if event.obj.message["text"] != "":
-            member = vk.groups.isMember(group_id=225254028, user_id=event.obj.message['from_id'])
-            if member == 0:
-                vk.messages.send(user_id=event.obj.message['from_id'],
-                                 message=f"Привет, {vk.users.get(user_id=event.obj.message['from_id'], fields='first_name')[0]['first_name']}! Для прохождения опросов нужно подписаться на бота👆",
-                                 random_id=random.randint(0, 2 ** 64))
-            elif event.type == VkBotEventType.GROUP_JOIN and vk.users.get(user_id=event.object.user_id):
-                vk.messages.send(user_id=event.object.user_id,
-                                 message=f"{vk.users.get(user_id=event.object.user_id, fields='first_name')[0]['first_name']}, нет аккаунта? Зарегистрируйте его по этой ссылке: *ссылка*",
-                                 random_id=random.randint(0, 2 ** 64))
-                print(
-                    f"пользователь {vk.users.get(user_id=event.object.user_id, fields='first_name')[0]['first_name']} присоединился!")
-            elif event.type == VkBotEventType.MESSAGE_NEW:
+        if event.type == VkBotEventType.GROUP_JOIN and vk.users.get(user_id=event.object.user_id):
+            vk.messages.send(user_id=event.object.user_id,
+                             message=f"{vk.users.get(user_id=event.object.user_id, fields='first_name')[0]['first_name']}, нет аккаунта? Зарегистрируйте его по этой ссылке: *ссылка*",
+                             random_id=random.randint(0, 2 ** 64))
+            print(
+                f"пользователь {vk.users.get(user_id=event.object.user_id, fields='first_name')[0]['first_name']} присоединился!")
+            vk.messages.send(user_id=event.object.user_id,
+                             message=f"После регитсрации вы можете узнать информацию об акааунте, написав 'инфо'",
+                             random_id=random.randint(0, 2 ** 64))
+        elif event.type == VkBotEventType.MESSAGE_NEW:
+            print(event.obj.message['from_id'])
+            if event.obj.message['text'].lower() == 'инфо':
                 us = db_sess.query(User).filter(User.vk_id == event.obj.message['from_id']).first()
                 user_id = us.id
                 quizes = db_sess.query(Quezes).filter(Quezes.authorid == user_id).all()
                 vk.messages.send(user_id=event.obj.message['from_id'],
                                  message=f"{vk.users.get(user_id=event.obj.message['from_id'], fields='first_name')[0]['first_name']}, ты создал {len(quizes)} {com.make_agree_with_number(len(quizes)).word}.",
+                                 random_id=random.randint(0, 2 ** 64))
+            member = vk.groups.isMember(group_id=225254028, user_id=event.obj.message['from_id'])
+            if member == 0:
+                vk.messages.send(user_id=event.obj.message['from_id'],
+                                 message=f"Привет, {vk.users.get(user_id=event.obj.message['from_id'], fields='first_name')[0]['first_name']}! Для прохождения опросов нужно подписаться на бота👆",
                                  random_id=random.randint(0, 2 ** 64))
